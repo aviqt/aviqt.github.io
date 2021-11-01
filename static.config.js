b2aa7ef388cfc1753a274a8ea9e0e2d9b009ab23
王晓龙 <964589478@qq.com>
2021-11-01 17:52:52 +0800

    20211101

---
+++ static.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  NVRSERVER:{
+    szIP : '192.168.1.200',
+    szPort : '80',
+    szUsername : 'admin',
+    szPassword : '123456Aa'
+  },
+  NVRTESTDATA:{
+    iChannelID: '3',
+    szStartTime: "2020-10-19 16:10:00",
+    szEndTime: "2020-10-19 16:15:37",
+  }
+}
